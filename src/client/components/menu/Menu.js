cdd07d55c58c1dded8d6df3e1602eb5b2cb19473
David Chu <dchu26@ucmerced.edu>
2022-03-06 02:53:52 -0800

    Enhance share button user experience #5 (#18)
    * Changed icon
    
    * a glorious moment in time
    
    * Revert "a glorious moment in time"
    
    This reverts commit af6570df65cfbbc65612a813b81ad326fe85d574.
    
    * Fix

--- src/client/components/menu/Menu.js
+++ src/client/components/menu/Menu.js
@@ -87,9 +87,9 @@ const Menu = () => {
         </div>
         <div className="share" onClick={onShare}>
           {isShared ? (
-            <ion-icon name="share-social-sharp" />
+            <ion-icon name="copy" />
           ) : (
-            <ion-icon name="share-social" />
+            <ion-icon name="copy-outline" />
           )}
         </div>
         <div className="mute" onClick={onSound}>
