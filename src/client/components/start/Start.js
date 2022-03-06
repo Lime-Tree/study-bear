33229b3a65e698328e741a4df8b6257209b18da4
Joe Yuan <yuan.joe12@gmail.com>
2022-03-06 11:18:44 +0000

    Add button info to start screen (#19)

--- src/client/components/start/Start.js
+++ src/client/components/start/Start.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { SocketContext } from '../../App.js';
 import './Start.css';
@@ -6,6 +6,8 @@ import './Start.css';
 const Start = () => {
   const socket = useContext(SocketContext);
   const navigate = useNavigate();
+  const [isCreateRoomHovered, setIsCreateRoomHovered] = useState(false);
+  const [isRandomRoomHovered, setIsRandomRoomHovered] = useState(false);
 
   useEffect(() => {
     socket.on('roomKey', (roomKey) => {
@@ -23,8 +25,44 @@ const Start = () => {
 
   return (
     <div className="start">
-      <ion-icon onClick={createRoom} name="home" />
-      <ion-icon onClick={randomRoom} name="dice" />
+      <div className="hidden padding">Padding</div>
+      <div
+        className={`create-room-text button-info ${
+          isCreateRoomHovered ? '' : 'hidden'
+        }`}
+      >
+        Make a room
+      </div>
+      <div className="start-buttons">
+        <ion-icon
+          onMouseOver={() => {
+            setIsCreateRoomHovered(true);
+          }}
+          onMouseOut={() => {
+            setIsCreateRoomHovered(false);
+          }}
+          onClick={createRoom}
+          name="home"
+        />
+        <ion-icon
+          onMouseOver={() => {
+            setIsRandomRoomHovered(true);
+          }}
+          onMouseOut={() => {
+            setIsRandomRoomHovered(false);
+          }}
+          onClick={randomRoom}
+          name="dice"
+        />
+      </div>
+      <div
+        className={`random-room-text button-info ${
+          isRandomRoomHovered ? '' : 'hidden'
+        }`}
+      >
+        Visit a room
+      </div>
+      <div className="hidden padding">Padding</div>
     </div>
   );
 };
